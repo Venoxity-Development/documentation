85d64967c49986baf15dccfad46518c25c53b1b2
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-05-02 05:22:10 +0000

    f

--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,15 @@
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
 
 export const { docs, meta } = defineDocs({
   dir: 'content/docs',
 });
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkMath],
+    // Place it at first so that it won't be changed by syntax highlighter
+    rehypePlugins: (v) => [rehypeKatex, ...v],
+  },
+});
