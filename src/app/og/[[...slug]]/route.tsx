2cdb7b8719000cd7012dacb1c53c5c62b17d89a9
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2025-08-28 01:42:00 -0700

    refactor: broken code

--- src/app/og/[[...slug]]/route.tsx
+++ src/app/og/[[...slug]]/route.tsx
@@ -1,52 +1,52 @@
-import { readFileSync } from "node:fs";
-import { notFound } from "next/navigation";
-import { generateOGImage } from "@/app/og/[[...slug]]/og";
-import { source } from "@/lib/source";
+import { readFileSync } from 'node:fs'
+import { notFound } from 'next/navigation'
+import { generateOGImage } from '@/app/og/[[...slug]]/og'
+import { source } from '@/lib/source'
 
-const font = readFileSync("./src/app/og/[[...slug]]/fonts/Inter-Regular.ttf");
+const font = readFileSync('./src/app/og/[[...slug]]/fonts/Inter-Regular.ttf')
 const fontSemiBold = readFileSync(
-  "./src/app/og/[[...slug]]/fonts/Inter-SemiBold.ttf"
-);
-const fontBold = readFileSync("./src/app/og/[[...slug]]/fonts/Inter-Bold.ttf");
+  './src/app/og/[[...slug]]/fonts/Inter-SemiBold.ttf'
+)
+const fontBold = readFileSync('./src/app/og/[[...slug]]/fonts/Inter-Bold.ttf')
 
 export async function GET(
   _req: Request,
-  { params }: RouteContext<"/og/[[...slug]]">
+  { params }: RouteContext<'/og/[[...slug]]'>
 ) {
-  const { slug } = await params;
-  const page = source.getPage(slug.slice(0, -1));
-  if (!page) notFound();
+  const slug = (await params).slug
+  const page = source.getPage(slug)
+  if (!page) notFound()
 
   return generateOGImage({
-    primaryTextColor: "rgb(240,240,240)",
+    primaryTextColor: 'rgb(240,240,240)',
     title: page.data.title,
     description: page.data.description,
     tag: page.slugs[0],
     fonts: [
       {
-        name: "Inter",
+        name: 'Inter',
         data: font,
         weight: 400,
       },
       {
-        name: "Inter",
+        name: 'Inter',
         data: fontSemiBold,
         weight: 600,
       },
       {
-        name: "Inter",
+        name: 'Inter',
         data: fontBold,
         weight: 700,
       },
     ],
-  });
+  })
 }
 
 export function generateStaticParams(): {
-  slug: string[];
+  slug: string[]
 }[] {
   return source.generateParams().map((page) => ({
     ...page,
-    slug: [...page.slug, "image.png"],
-  }));
+    slug: [...page.slug, 'image.png'],
+  }))
 }
