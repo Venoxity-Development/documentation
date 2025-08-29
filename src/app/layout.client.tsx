7a7f76c7a9075d0da525a2ecfaa3cdb11609c16e
Anirudh Sriram <hello@techwithanirudh.com>
2025-08-29 12:24:31 +0000

    feat: enhance Body component to accept page tree for mode determination
    - Updated the Body component to accept a PageTree.Root parameter for improved mode handling.
    - Introduced ROOTS constant for top-level directory identification.
    - Modified useMode function to derive the mode based on the provided page tree structure.

--- src/app/layout.client.tsx
+++ src/app/layout.client.tsx
@@ -1,11 +1,23 @@
 'use client'
 
+import type { PageTree } from 'fumadocs-core/server'
 import { useParams } from 'next/navigation'
 import type { ReactNode } from 'react'
 import { cn } from '@/lib/cn'
 
-export function Body({ children }: { children: ReactNode }) {
-  const mode = useMode()
+/**
+ * Roots are the top-level directories that can be used to determine the mode
+ */
+export const ROOTS = ['changelog', 'api-reference']
+
+export function Body({
+  children,
+  tree,
+}: {
+  children: ReactNode
+  tree: PageTree.Root
+}) {
+  const mode = useMode(tree)
 
   return (
     <body className={cn(mode, 'relative flex min-h-screen flex-col')}>
@@ -14,7 +26,10 @@ export function Body({ children }: { children: ReactNode }) {
   )
 }
 
-export function useMode(): string | undefined {
+export function useMode(tree: PageTree.Root): string {
   const { slug } = useParams()
-  return Array.isArray(slug) && slug.length > 0 ? slug[0] : undefined
+  const root = Array.isArray(slug) && slug.length > 0 ? slug[0] : 'app'
+  const page = tree.getPage(root)
+
+  return page?.data._file.path.split('/').slice(0, -1).join('/') ?? 'app'
 }
